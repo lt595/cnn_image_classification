507deb1b6958f99885d43ec7f397c1498f141a3e
lt595 <ltarts95@gmail.com>
2024-10-21 02:17:42 +1300

    adjust for 1 epochs only

--- cnn_model.py
+++ cnn_model.py
@@ -63,7 +63,7 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
 # Training loop
-num_epochs = 5
+num_epochs = 1
 for epoch in range(num_epochs):
     model.train()
     running_loss = 0.0
